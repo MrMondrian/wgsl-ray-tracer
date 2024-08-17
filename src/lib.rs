7e5e32d9fa1c0d7e9ae08f7b625f899028af5365
Author Name <anthony.tan2@mail.mcgill.ca>
2024-08-17 17:39:02 -0500

    resize triangles to whole screen

--- src/lib.rs
+++ src/lib.rs
@@ -221,27 +221,27 @@ impl Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-0.5, 0.5],
+        position: [-1.0, 1.0],
         tex_coords: [0.0, 0.0],
     },
     Vertex {
-        position: [-0.5, -0.5],
+        position: [-1.0, -1.0],
         tex_coords: [0.0, 1.0],
     },
     Vertex {
-        position: [0.5, -0.5],
+        position: [1.0, -1.0],
         tex_coords: [1.0, 1.0],
     },
     Vertex {
-        position: [-0.5, 0.5],
+        position: [-1.0, 1.0],
         tex_coords: [0.0, 0.0],
     },
     Vertex {
-        position: [0.5, -0.5],
+        position: [1.0, -1.0],
         tex_coords: [1.0, 1.0],
     },
     Vertex {
-        position: [0.5, 0.5],
+        position: [1.0, 1.0],
         tex_coords: [1.0, 0.0],
     },
 ];
