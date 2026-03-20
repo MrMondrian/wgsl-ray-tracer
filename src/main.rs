b8c1176a01b2758f2b84112aac8e3c0ae3a623c8
Anthony Tan <anthonytan8@protonmail.com>
2026-03-19 23:50:16 -0500

    spherical skybox

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use wgsl::ray_tracer;
-// use wgsl::hitable::*;
-// use nalgebra::Vector3;
+use wgsl::{ray_tracer, Shader};
 
 #[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
 pub fn main() {
+    let shader = std::env::args()
+        .find(|a| a == "--spherical")
+        .map(|_| Shader::Spherical)
+        .unwrap_or(Shader::Cartesian);
 
-    ray_tracer();
+    ray_tracer(shader);
 }
